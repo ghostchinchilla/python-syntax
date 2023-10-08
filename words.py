90696ac710267726a37fad80895d96d518d612a2
ghostchinchilla <133160254+ghostchinchilla@users.noreply.github.com>
2023-10-08 10:42:06 -0400

    Add files via upload

---
+++ words.py
@@ -0,0 +1,19 @@
+# Step Two: Starting On Your Own
+
+words = ["egg", "raccon", "elephant", "squrirrel", "edamame"]
+
+def print_upper_word(words):
+    print (f"{words}.upper()")
+
+def print_e(words):
+    for word in words:
+        if word.startswith('e'):
+            print(words)  
+
+
+
+letters = []
+
+for word in letters:
+    if word[0] == letters:
+        print (word)
